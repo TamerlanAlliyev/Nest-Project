3c1bc23c272e52c8dd3f45f1ed0109ac43c0fc2a
TamerlanAlliyev <tamerlanma@code.edu.az>
2024-03-07 00:06:12 +0400

    Added Home and Product View

---
+++ Nest/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Nest.Controllers
+{
+	public class HomeController : Controller
+	{
+		public IActionResult Index()
+		{
+            ViewBag.PreloaderPartialView = true;
+            ViewBag.MobileHeaderPartialView = true;
+            ViewBag.QuickPartialView = true;
+            return View();
+		}
+	}
+}
---
+++ Nest/Controllers/ProductController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Nest.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.PreloaderPartialView = true;
+            ViewBag.MobileHeaderPartialView = true;
+            ViewBag.QuickPartialView = true;
+            return View();
+        }
+    }
+}
